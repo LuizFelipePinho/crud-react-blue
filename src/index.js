f7c698e156c4c2da76db0b977f01d73b1218ac39
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-09-13 22:30:58 -0300

    oonde eu parei

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { browserRouter } from 'react-router-dom';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <browserRouter>
+    
+      <App />
+    
+    </browserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
+// parei em 1:07
